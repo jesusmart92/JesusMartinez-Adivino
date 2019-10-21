46b919ca6ebe43d6809053a733eb13e92069d5ed
jesus <jesus@jesus-VirtualBox>
2019-10-21 09:55:59 +0200

    Cambiado el rango de numeros para acertar entre 10 y 50

--- src/main/java/com/mycompany/jesusmartinezadivino/JesusMartinezAdivino.java
+++ src/main/java/com/mycompany/jesusmartinezadivino/JesusMartinezAdivino.java
@@ -14,7 +14,7 @@ public class JesusMartinezAdivino {
     
      public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random() * 40 + 10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
